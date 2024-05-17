5be5a570266e02294356922c89d1a5253ab0a171
zhangyibo111 <1463448053@qq.com>
2024-05-17 17:45:04 +0800

    list3

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,7 +5,7 @@ import search from "@/views/search.vue";
 import list from "@/views/list.vue";
 // 数组写法
 const routes: Array<RouteRecordRaw> = [
-  { path: "/", redirect: "/a" },
+  { path: "/", redirect: "/list" },
   { path: "/a", component: A },
   { path: "/b", component: B },
   { path: "/search", component: search },
